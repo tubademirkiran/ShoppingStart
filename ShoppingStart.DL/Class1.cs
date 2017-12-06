aa262b5229e8c0088255f1573037af8d387afa5c
Tuba Demirkıran <tubademirkiran95@hotmail.com>
2017-12-06 23:13:36 +0300

    Add project files.

---
+++ ShoppingStart.DL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingStart.DL
+{
+    public class Class1
+    {
+    }
+}
